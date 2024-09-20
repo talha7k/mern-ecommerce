035c5d95272c4537cd5745cad5549fde19545b7f
Talha7k <talha7k@gmail.com>
2024-09-20 22:37:35 +0530

    modified the admin assets

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -2,7 +2,7 @@ import logo from "./logo.png";
 import add_icon from "./add_icon.png";
 import order_icon from "./order_icon.png";
 import upload_area from "./upload_area.png";
-import parcel_icon from "./parcel_icon.svg";
+import parcel_icon from "./parcel_icon.png";
 
 export const assets = {
   logo,
